b63c8f62ce84471aa145dddf0cfc2ddd059d9265
unknown <VAIO Roberth@VAIORoberth-PC.(none)>
2012-04-23 09:52:57 -0400

    TareaNo1

---
+++ tarea/tarea/Controllers/calcularController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tarea.Models;
+
+namespace tarea.Controllers
+{
+    public class calcularController : Controller
+    {
+        //
+        // GET: /calcular/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult sumar()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult sumar(calculo model)
+        {
+            int r = model.a + model.b;
+            ViewBag.resultado = r;
+            return View();
+        }
+        public ActionResult restar()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult restar(calculo model)
+        {
+            int r = model.a - model.b;
+            ViewBag.resultado1 = r;
+            return View();
+        }
+        public ActionResult multiplicar()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult multiplicar(calculo model)
+        {
+            int r = model.a * model.b;
+            ViewBag.resultado2 = r;
+            return View();
+        }
+        public ActionResult dividir()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult dividir(calculo model)
+        {
+            if(model.d!=0)
+            {   float r = model.c / model.d;
+            ViewBag.resultado3 = r;
+            return View();
+        }
+            else
+            ViewBag.resultado3="Error division entre cero";
+            return View();
+        }
+    }
+}
---
+++ tarea/tarea/Models/calculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tarea.Models
+{
+    public class calculo
+    {
+        public int a { set; get; }
+        public int b { set; get; }
+        public float c { set; get; }
+        public float d { set; get; }
+    }
+}
